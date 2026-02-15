d78c71792f340bd034eccebd93c261a2879da68c
Demo-23home <zeyadslama23@gmail.com>
2026-02-15 22:16:54 +0200

    feat(core): implement wait_for_db management command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,8 +1,12 @@
 """
 Django command to make sure PostgreSQL is available.
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
 
 
 class Command(BaseCommand):
@@ -11,4 +15,19 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        pass
+        """
+        Entry Point for command.
+        """
+        self.stdout.write("Waiting for database...")
+
+        db_up = False 
+        
+        while db_up is False: 
+            try: 
+                self.check(databases=["default"])
+                db_up = True
+            except(OperationalError, Psycopg2OpError): 
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+                
+        self.stdout.write(self.style.SUCCESS("Database available!"))
