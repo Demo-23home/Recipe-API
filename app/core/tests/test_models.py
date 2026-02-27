4f6cc056d54796df845be940345829c210e8a944
Demo-23home <zeyadslama23@gmail.com>
2026-02-27 10:12:43 +0200

    test(core): add create recipe model test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,8 +2,12 @@
 Tests for models.
 """
 
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -57,3 +61,24 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """
+        Test create recipe is successful.
+        """
+
+        user = get_user_model().objects.create(
+            email="testemail@exmaple.com",
+            password="TestPassword@123",
+            name="Test Users",
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Test Recipe",
+            time_minutes=5,
+            price=Decimal(5.50),
+            description="Simple recipe description"
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
