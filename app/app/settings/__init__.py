81442b12291254cb3676ea242ef5cb7017d6a9b3
Demo-23home <zeyadslama23@gmail.com>
2026-03-16 16:11:18 +0200

    remove dot-env

--- app/app/settings/__init__.py
+++ app/app/settings/__init__.py
@@ -1,11 +1,7 @@
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Always attempt to load .env (safe if it doesn't exist)
-load_dotenv(BASE_DIR / ".env")
 
 # Now read environment
 ENV = os.getenv("DJANGO_ENV", "prod")
